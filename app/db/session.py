d6be5ca9ef7486b18f64f0dbff7748a6c39c1345
Jiang Zhenggang <1321670187@qq.com>
2025-08-20 13:33:53 +0800

    优化连接池

--- app/db/session.py
+++ app/db/session.py
@@ -1,14 +1,16 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
+from tenacity import retry, stop_after_attempt
 import logging
 
 logger = logging.getLogger(__name__)
 
 DATABASE_URL = f"postgresql://{settings.DB_USERNAME}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_DATABASE}"
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_recycle=3600, pool_timeout=30)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+@retry(stop=stop_after_attempt(3))
 def get_db():
     db = SessionLocal(bind=engine)
     try:
